03ddf7e925f109388300f4fa6279740b4bfa9324
Beinsezii <beinsezii@gmail.com>
2023-10-31 16:12:49 -0700

    dtype choice and fix colors for realsies
    use euler's max sigma to div down the latent cause i guess that's what
    it needs

--- quickdif.py
+++ quickdif.py
@@ -1,5 +1,5 @@
-COLS_XL = {'black': [-0.17645598948001862, 0.03146037831902504, 0.019621476531028748, 0.02099631540477276], 'white': [0.14688679575920105, 0.014052464626729488, 0.07579405605792999, -0.06623126566410065], 'red': [-0.16008996963500977, -0.16115550696849823, 0.08697202056646347, -0.10116755962371826], 'green': [-0.028744127601385117, 0.11458607017993927, 0.2156391739845276, 0.07063306123018265], 'blue': [0.003709609853103757, 0.13306322693824768, -0.14386092126369476, 0.0337492972612381], 'cyan': [0.10122263431549072, 0.21176232397556305, 0.03499620035290718, 0.06475268304347992], 'magenta': [-0.007828177884221077, -0.041593436151742935, -0.09819445759057999, -0.07344077527523041], 'yellow': [-0.053803637623786926, -0.08599691092967987, 0.2644002437591553, -0.06683327257633209]}
-COLS_FTMSE = {'black': [-0.06220785155892372, -0.1626504510641098, 0.06971073150634766, 0.08103783428668976], 'white': [0.1359347701072693, 0.0902138352394104, -0.0019893920980393887, -0.07263711094856262], 'red': [0.07859428226947784, -0.054802559316158295, -0.11742548644542694, 0.04870118200778961], 'green': [0.04800793528556824, 0.051367077976465225, 0.14700517058372498, 0.1133447140455246], 'blue': [-0.039562880992889404, -0.1904129981994629, 0.06180213391780853, -0.08763758838176727], 'cyan': [0.03642553836107254, -0.006509925238788128, 0.18342071771621704, -0.006117651239037514], 'magenta': [0.046169131994247437, -0.09723665565252304, -0.08496242761611938, -0.08991505205631256], 'yellow': [0.14672884345054626, 0.1251959651708603, -0.004944938700646162, 0.10380695760250092]}
+COLS_XL = {'black': [-2.8232955932617188, 0.5033664703369141, 0.3139435052871704, 0.3359411954879761], 'white': [2.3501884937286377, 0.224839448928833, 1.2127048969268799, -1.0597002506256104], 'red': [-2.561439037322998, -2.5784878730773926, 1.3915523290634155, -1.6186809539794922], 'green': [-0.45990604162216187, 1.8333771228790283, 3.4502267837524414, 1.1301288604736328], 'blue': [0.05935399606823921, 2.129012107849121, -2.3017752170562744, 0.5399889349937439], 'cyan': [1.619562029838562, 3.388197183609009, 0.5599392056465149, 1.0360429286956787], 'magenta': [-0.12525132298469543, -0.6654946208000183, -1.5711115598678589, -1.175052523612976], 'yellow': [-0.8608582019805908, -1.375950574874878, 4.230403900146484, -1.0693323612213135]}
+COLS_FTMSE = {'black': [-0.9953256249427795, -2.602407455444336, 1.1153717041015625, 1.2966053485870361], 'white': [2.1749563217163086, 1.4434212446212769, -0.03183012455701828, -1.162193775177002], 'red': [1.2575085163116455, -0.8768408298492432, -1.8788076639175415, 0.7792190313339233], 'green': [0.768126904964447, 0.821873128414154, 2.3520827293395996, 1.8135154247283936], 'blue': [-0.6330060958862305, -3.0466079711914062, 0.9888341426849365, -1.4022014141082764], 'cyan': [0.5828086137771606, -0.10415874421596527, 2.934731960296631, -0.09788236021995544], 'magenta': [0.7387062311172485, -1.555786371231079, -1.3593989610671997, -1.438640832901001], 'yellow': [2.3476614952087402, 2.0031352043151855, -0.079119011759758, 1.6609115600585938]}
 
 
 import argparse
@@ -17,6 +17,7 @@ parser.add_argument('-c', '--color', choices=list(COLS_XL.keys()), default='blac
 parser.add_argument('-C', '--color_scale', type=float, default=0.0)
 parser.add_argument('-m', '--model', type=str, default="stabilityai/stable-diffusion-xl-base-1.0")
 parser.add_argument('-o', '--out', type=Path, default="/tmp/quickdif/")
+parser.add_argument('-d', '--dtype', choices=["fp16", "bf16", "fp32"], default="fp16")
 parser.add_argument('--seed', type=int, default=-1)
 parser.add_argument('--dpm', action='store_true')
 parser.add_argument('--compile', action='store_true')
@@ -37,11 +38,13 @@ from diffusers import (
     StableDiffusionXLPipeline,
     DDIMScheduler,
     DPMSolverMultistepScheduler,
+    EulerDiscreteScheduler,
 )
 from compel import Compel, ReturnedEmbeddingsType
 torch.set_float32_matmul_precision('high')
+dtype = {"fp16":torch.float16, "bf16":torch.bfloat16, "fp32":torch.float32}[args.dtype]
 
-pipe_args = {'torch_dtype':torch.float16, 'use_safetensors':True, 'add_watermarker':False}
+pipe_args = {'torch_dtype':dtype, 'use_safetensors':True, 'add_watermarker':False}
 
 if args.model.endswith('.safetensors'):
     try:
@@ -64,12 +67,16 @@ else:
     compel = Compel(tokenizer=pipe.tokenizer, text_encoder=pipe.text_encoder, truncate_long_prompts= False)
 
 pipe.safety_checker = None
+if dtype != torch.float16:
+    pipe.vae.force_upcast = False
 
 if args.compile:
     pipe.unet = torch.compile(pipe.unet)
 else:
     pipe.unet.set_default_attn_processor()
     pipe.vae.set_default_attn_processor()
+
+sigma = EulerDiscreteScheduler.from_config(pipe.scheduler.config, timestep_spacing='trailing').init_noise_sigma
 
 if args.dpm:
     pipe.scheduler = DPMSolverMultistepScheduler.from_config(
@@ -89,9 +96,9 @@ generator = torch.manual_seed(args.seed) if args.seed >= 0 else torch.default_ge
 size = [1, 4, args.height // 8, args.width // 8]
 
 if args.color_scale <= 0.0:
-    latents = torch.zeros(size, dtype=torch.float16, device='cpu')
+    latents = torch.zeros(size, dtype=dtype, device='cpu')
 else:
-    latents = torch.tensor(COLS_XL[args.color] if XL else COLS_FTMSE[args.color], dtype=torch.float16, device='cpu').mul(args.color_scale).expand([size[0], size[2], size[3], size[1]]).permute((0, 3, 1, 2)).clone()
+    latents = torch.tensor(COLS_XL[args.color] if XL else COLS_FTMSE[args.color], dtype=dtype, device='cpu').mul(args.color_scale).div(sigma).expand([size[0], size[2], size[3], size[1]]).permute((0, 3, 1, 2)).clone()
 
 # f32 noise for equal seeds amongst other UIs
 latents += torch.randn(latents.shape, generator=generator, dtype=torch.float32)
--- vaecols.py
+++ vaecols.py
@@ -4,7 +4,6 @@ from pathlib import Path
 parser = argparse.ArgumentParser(description='Measure cardinal colors on VAE')
 parser.add_argument('-v', '--vae', type=str, default='stabilityai/sdxl-vae')
 parser.add_argument('-s', '--size', type=int, default=1024)
-parser.add_argument('-N', '--noscale', action='store_true')
 args = parser.parse_args()
 
 import torch, diffusers
@@ -12,10 +11,7 @@ from diffusers import image_processor
 
 vae = diffusers.AutoencoderKL.from_pretrained(str(args.vae), use_safetensors=True)
 vae.set_default_attn_processor()
-vae_scale = 2 ** (len(vae.config.block_out_channels) - 1)
-processor = image_processor.VaeImageProcessor(vae_scale)
-
-print(f"factor: {vae.config.scaling_factor}, scale: {vae_scale}")
+processor = image_processor.VaeImageProcessor(2 ** (len(vae.config.block_out_channels) - 1))
 
 results = {}
 
@@ -31,9 +27,7 @@ for col, pix in [
 ]:
     img = torch.tensor(pix, dtype=torch.float32).expand([1, args.size, args.size, 3]).permute((0, 3, 1, 2)).clone()
     img = processor.preprocess(img)
-    tensor = vae.to('cuda').encode(img.to('cuda')).latent_dist.sample()
-    if not args.noscale:
-        tensor *= vae.config.scaling_factor / vae_scale / 2
+    tensor = vae.to('cuda').encode(img.to('cuda')).latent_dist.sample() * vae.config.scaling_factor
     results[col] = [c.mean().item() for c in tensor[0]]
 
     del img, tensor
