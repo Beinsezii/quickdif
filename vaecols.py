9841cf15831144a1200d25ab15787cef04541390
Beinsezii <beinsezii@gmail.com>
2023-10-31 14:11:52 -0700

    vaecols noscale flag

--- vaecols.py
+++ vaecols.py
@@ -4,6 +4,7 @@ from pathlib import Path
 parser = argparse.ArgumentParser(description='Measure cardinal colors on VAE')
 parser.add_argument('-v', '--vae', type=str, default='stabilityai/sdxl-vae')
 parser.add_argument('-s', '--size', type=int, default=1024)
+parser.add_argument('-N', '--noscale', action='store_true')
 args = parser.parse_args()
 
 import torch, diffusers
@@ -31,7 +32,9 @@ for col, pix in [
 ]:
     img = torch.tensor(pix, dtype=torch.float32).expand([1, args.size, args.size, 3]).permute((0, 3, 1, 2)).clone()
     img = processor.preprocess(img)
-    tensor = vae.to('cuda').encode(img.to('cuda')).latent_dist.sample().to('cpu') * (vae.config.scaling_factor ** vae_scale * 10 ** power)
+    tensor = vae.to('cuda').encode(img.to('cuda')).latent_dist.sample()
+    if not args.noscale:
+        tensor *= (vae.config.scaling_factor ** vae_scale * 10 ** power)
     results[col] = [c.mean().item() for c in tensor[0]]
 
     del img, tensor
