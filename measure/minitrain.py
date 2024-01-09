f6e5cff4137e394759fa0ce6445e8a9ecf7dfd0c
Beinsezii <beinsezii@gmail.com>
2024-01-08 20:21:49 -0800

    minitrain use adamw grad optimizer

--- measure/minitrain.py
+++ measure/minitrain.py
@@ -6,73 +6,13 @@ import argparse
 
 torch.set_default_dtype(torch.float32)
 torch.set_default_device('cuda')
-torch.set_grad_enabled(False)
-torch.set_printoptions(precision=4)
-
-def evaluate(channel, latent, tensors, functions):
-    l = latent.clone()
-    for tensor, function in zip(tensors, functions): l = function(l, tensor)
-    diff = torch.absolute(channel - l)
-    cumdev = (((diff + 1) ** 2) - 1).sum()
-    return cumdev
-
-
-def train(channel, latent, tensors, functions, steps, rate):
-    steps = int(steps)
-    dev = evaluate(channel, latent, tensors, functions)
-
-    tensors_best = [tensor.clone() for tensor in tensors]
-    dev_best = dev.clone()
-
-    bar = tqdm(total=steps)
-    update = 1000
-    for i in range(steps):
-        tensors = [tensor + torch.randn_like(tensor) * rate for tensor in tensors_best]
-        dev = evaluate(channel, latent, tensors, functions)
-        if dev < dev_best: tensors_best, dev_best = tensors, dev
-        if (i+1) % update == 0:
-            bar.update(update)
-            bar.set_description(f"{dev_best:.4f}")
-    return tensors_best, dev_best
-
-def sharpen(channel, latent, tensors, functions):
-    rate = 10.0
-    dev = evaluate(channel, latent, tensors, functions)
-    bar = tqdm()
-    for r in range(6):
-        while True:
-            change = False
-            for tensor in tensors:
-                for x in range(tensor.shape[0]):
-                    for y in range(tensor.shape[1]):
-                        tensor[x,y] += rate
-                        ndev = evaluate(channel, latent, tensors, functions)
-                        if ndev < dev:
-                            dev = ndev
-                            change = True
-                            continue
-                        else:
-                            tensor[x,y] += rate * -2.0
-                        ndev = evaluate(channel, latent, tensors, functions)
-                        if ndev < dev:
-                            dev = ndev
-                            change = True
-                            continue
-                        else: tensor[x,y] += rate
-            bar.update()
-            bar.set_description(f"{dev:.4f}")
-            if not change: break
-        rate /= 10.0
-    return tensors, dev
-
-
+torch.set_printoptions(precision=12)
 
 parser = argparse.ArgumentParser(description='Map latent channels to CIELAB')
 parser.add_argument('data', type=argparse.FileType(mode='r'))
 args = parser.parse_args()
 
 data = json.load(args.data)
-
 assert data['index_space'] == 'SRGB'
 
 latent = torch.tensor(list(map(
@@ -109,16 +49,39 @@ srgb = torch.tensor(list(map(
 
 del data
 
-iterations = [
-    (2e+5, 1e-1),
-    (2e+5, 1e-2),
-    (2e+5, 1e-3),
+tensors = [
+    torch.nn.Parameter(torch.randn([4, 3])),
+    torch.nn.Parameter(torch.zeros([1, 3])),
+]
+functions = [
+    torch.matmul,
+    torch.add,
 ]
 
-functions = [torch.add, torch.matmul]
-tensors = [torch.randn([1,4]), torch.randn([4,3])]
+optimizer = torch.optim.AdamW(tensors, lr=1e-3)
 
-for (steps, rate) in iterations: tensors, dev = train(srgb, latent, tensors, functions, steps, rate)
-for _ in range(5): tensors, dev = sharpen(srgb, latent, tensors, functions)
+source = torch.flatten(latent, end_dim=2)
+target = torch.flatten(srgb, end_dim=2)
 
-print(f"###\n{tensors}\n###")
+steps = int(1e+5)
+update = 100
+bar = tqdm(total=steps)
+
+try:
+    for epoch in range(steps):
+        prediction = source.clone()
+        for (t, f) in zip(tensors, functions): prediction = f(prediction, t)
+        loss = torch.absolute((prediction - target) ** 2).sum()
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if (epoch+1) % update == 0:
+            bar.update(update)
+            bar.set_description(f"Epoch {epoch+1} Loss {loss:.4f}")
+except KeyboardInterrupt:
+    print()
+
+print("###\n")
+for tensor in tensors: print(f"{tensor}\n")
+print("###")
