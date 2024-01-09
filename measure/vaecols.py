62d8f9c10e7cd396208498d9c57c25c952390b55
Beinsezii <beinsezii@gmail.com>
2024-01-08 22:13:35 -0800

    vaecols.py vastly simplify & improve mapping

--- measure/vaecols.py
+++ measure/vaecols.py
@@ -1,133 +1,46 @@
-try:
-    import ctypes
-    cpixel = ctypes.c_float * 3
-    colcon = ctypes.CDLL("./libcolcon.so")
-
-    # up
-    colcon.srgb_to_hsv.argtypes = [cpixel]
-    colcon.srgb_to_lrgb.argtypes = [cpixel]
-    colcon.lrgb_to_xyz.argtypes = [cpixel]
-    colcon.xyz_to_lab.argtypes = [cpixel]
-    colcon.lab_to_lch.argtypes = [cpixel]
-
-    # down
-    colcon.lch_to_lab.argtypes = [cpixel]
-    colcon.lab_to_xyz.argtypes = [cpixel]
-    colcon.xyz_to_lrgb.argtypes = [cpixel]
-    colcon.lrgb_to_srgb.argtypes = [cpixel]
-    colcon.srgb_to_hsv.argtypes = [cpixel]
-
-    # extra
-    colcon.expand_gamma.argtypes = [ctypes.c_float]
-    colcon.expand_gamma.restype = ctypes.c_float
-    colcon.correct_gamma.argtypes = [ctypes.c_float]
-    colcon.correct_gamma.restype = ctypes.c_float
-    colcon.hk_comp_2023.argtypes = [cpixel]
-
-    import json
-
-    has_colcon = True
-except Exception:
-    has_colcon = False
-
-
-import argparse
-
-parser = argparse.ArgumentParser(description='Measure cardinal colors on VAE')
-parser.add_argument('-v', '--vae', type=str, default='stabilityai/sdxl-vae')
-parser.add_argument('-r', '--raw', action='store_true', help='Return raw/unscaled values')
-if has_colcon: parser.add_argument('--lab', type=argparse.FileType(mode='w'), help="Output LAB map as .json")
-if has_colcon: parser.add_argument('--rgb', type=argparse.FileType(mode='w'), help="Output SRGB map as .json")
-args = parser.parse_args()
-
-import torch, diffusers
+import torch, diffusers, argparse, json
 from tqdm import tqdm
 from diffusers import image_processor
 
+parser = argparse.ArgumentParser(description='Measure cardinal colors on VAE')
+parser.add_argument('-v', '--vae', type=str, default='stabilityai/sdxl-vae')
+parser.add_argument('json', type=argparse.FileType(mode='w'), help="Output map as .json")
+args = parser.parse_args()
+
 dtype = torch.float32
+device = 'cuda'
+torch.set_default_dtype(dtype)
+torch.set_default_device(device)
 torch.set_grad_enabled(False)
 torch.set_float32_matmul_precision('high')
 
-vae = diffusers.AutoencoderKL.from_pretrained(str(args.vae), use_safetensors=True, torch_dtype=dtype).to('cuda')
+vae = diffusers.AutoencoderKL.from_pretrained(str(args.vae), use_safetensors=True, torch_dtype=dtype).to(device)
 processor = image_processor.VaeImageProcessor(2**(len(vae.config.block_out_channels) - 1))
 
-if args.lab:
-    scale = 5
-    iters = (100 // scale) + 1
-    data = [[[{} for _ in range(iters)] for _ in range(iters)] for _ in range(iters)]
-    bar = tqdm(total=iters**3)
-    for L in range(iters):
-        for A in range(iters):
-            for B in range(iters):
-                result = {}
-                pix = cpixel(L * scale, A * scale, B * scale)
+iters = torch.combinations(torch.linspace(0, 1, 26), r=3, with_replacement=True)
+iters = iters.reshape([iters.shape[0], 1, 1, iters.shape[1]]
+            ).expand([iters.shape[0], vae.config.sample_size, vae.config.sample_size, iters.shape[1]])
 
-                lch = cpixel(*pix)
-                colcon.lab_to_lch(lch)
-                result['LCH'] = list(lch)
-                result['LAB'] = list(pix)
-                colcon.lab_to_xyz(pix)
-                result['XYZ'] = list(pix)
-                colcon.xyz_to_lrgb(pix)
-                result['LRGB'] = list(pix)
-                colcon.lrgb_to_srgb(pix)
-                result['SRGB'] = list(pix)
+data = []
 
-                img = torch.tensor(pix, dtype=dtype).clamp(min=0.0, max=1.0).expand([1, vae.config.sample_size, vae.config.sample_size, 3]).permute((0, 3, 1, 2)).clone()
-                img = processor.preprocess(img).to('cuda')
-                tensor = vae.encode(img).latent_dist.sample()
-                result['latent'] = [c.mean().item() for c in tensor[0]]
-                data[L][A][B] = result
-                bar.update()
-    json.dump({'index_scale': scale, 'index_space': 'LAB', 'vae_scale_factor': vae.config.scaling_factor, 'data': data}, args.lab)
+for img in tqdm(iters):
+    result = {'rgb': img[0,0].tolist()}
+    # needs channels first
+    tensor = vae.encode(
+        processor.preprocess(img.permute(2,0,1))
+    ).latent_dist.sample().permute(1,0,2,3).flatten(start_dim=1)
 
-if args.rgb:
-    scale = 5
-    exp = 0.01
-    iters = (100 // scale) + 1
-    data = [[[{} for _ in range(iters)] for _ in range(iters)] for _ in range(iters)]
-    bar = tqdm(total=iters**3)
-    for R in range(iters):
-        for G in range(iters):
-            for B in range(iters):
-                result = {}
-                pix = cpixel(R * scale * exp, G * scale * exp, B * scale * exp)
-                img = torch.tensor(pix, dtype=dtype).clamp(min=0.0, max=1.0).expand([1, vae.config.sample_size, vae.config.sample_size, 3]).permute((0, 3, 1, 2)).clone()
-                img = processor.preprocess(img).to('cuda')
-                tensor = vae.encode(img).latent_dist.sample()
-                result['latent'] = [c.mean().item() for c in tensor[0]]
-                data[R][G][B] = result
+    result |= {
+        'latent_mean' : tensor.mean(dim=1).tolist(),
+        'latent_99' : tensor.quantile(0.99, dim=1).tolist(),
+        'latent_90' : tensor.quantile(0.90, dim=1).tolist(),
+        'latent_75' : tensor.quantile(0.75, dim=1).tolist(),
+        'latent_50' : tensor.quantile(0.50, dim=1).tolist(),
+        'latent_25' : tensor.quantile(0.25, dim=1).tolist(),
+        'latent_10' : tensor.quantile(0.10, dim=1).tolist(),
+        'latent_01' : tensor.quantile(0.01, dim=1).tolist(),
+    }
 
-                result['SRGB'] = list(pix)
-                colcon.srgb_to_lrgb(pix)
-                result['LRGB'] = list(pix)
-                colcon.lrgb_to_xyz(pix)
-                result['XYZ'] = list(pix)
-                colcon.xyz_to_lab(pix)
-                result['LAB'] = list(pix)
-                colcon.lab_to_lch(pix)
-                result['LCH'] = list(pix)
+    data.append(result)
 
-                bar.update()
-    json.dump({'index_scale': scale, 'index_space': 'SRGB', 'vae_scale_factor': vae.config.scaling_factor, 'data': data}, args.rgb)
-
-if not (args.rgb or args.lab):
-    results = {}
-
-    for col, pix in [
-        ("black", (0.0, 0.0, 0.0)),
-        ("white", (1.0, 1.0, 1.0)),
-        ("red", (1.0, 0.0, 0.0)),
-        ("green", (0.0, 1.0, 0.0)),
-        ("blue", (0.0, 0.0, 1.0)),
-        ("cyan", (0.0, 1.0, 1.0)),
-        ("magenta", (1.0, 0.0, 1.0)),
-        ("yellow", (1.0, 1.0, 0.0)),
-    ]:
-        img = torch.tensor(pix, dtype=torch.float32).expand([1, vae.config.sample_size, vae.config.sample_size, 3]).permute((0, 3, 1, 2)).clone()
-        img = processor.preprocess(img)
-        tensor = vae.encode(img.to('cuda')).latent_dist.sample()
-        if not args.raw: tensor.mul_(vae.config.scaling_factor)
-        results[col] = [c.mean().item() for c in tensor[0]]
-
-    print(results)
+json.dump({'vae_factor': vae.config.scaling_factor, 'data': data}, args.json)
